e47d669b58b9dd1bdec173d9e2e0d44a6356d740
vxnwt846 <benhassenmahdi@gmail.com>
2026-07-16 23:10:40 +0100

    fix: define sys_prot_t for lwIP FreeRTOS sys_arch
    CI failed with unknown type name sys_prot_t in lwip/sys.h. Provide the critical-section typedef required when SYS_LIGHTWEIGHT_PROT is enabled.

--- inc/arch/sys_arch.h
+++ inc/arch/sys_arch.h
@@ -1,6 +1,7 @@
 #ifndef LWIP_ARCH_SYS_ARCH_H
 #define LWIP_ARCH_SYS_ARCH_H
 
+#include <stdint.h>
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
@@ -10,6 +11,8 @@ typedef SemaphoreHandle_t sys_sem_t;
 typedef SemaphoreHandle_t sys_mutex_t;
 typedef QueueHandle_t     sys_mbox_t;
 typedef TaskHandle_t      sys_thread_t;
+/* Critical-section nesting token returned by sys_arch_protect() */
+typedef uint32_t          sys_prot_t;
 
 #define SYS_MBOX_NULL  ((sys_mbox_t)0)
 #define SYS_SEM_NULL   ((sys_sem_t)0)
